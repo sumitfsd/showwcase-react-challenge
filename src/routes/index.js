0ed4836a5ba191017e876b6450e02568552ee05d
Sumit Rajput <sumitfsd@gmail.com>
2020-05-26 16:54:29 +0530

    Add route for qualification page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Homepage from '../containers/Homepage';
+import Qualifications from '../containers/Qualifications';
 import { ROUTES } from '../constant';
 
 const routes = (
   <Switch>
     <Route path={ROUTES.HOMEPAGE} exact component={Homepage} />
+    <Route path={ROUTES.QUALIFICATIONS} exact component={Qualifications} />
     <Redirect from='/*' to={ROUTES.HOMEPAGE} />
   </Switch>
 );
