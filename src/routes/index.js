331ad2f9432cc49e6c4d832b99b9e65090082b17
Sumit Rajput <sumitfsd@gmail.com>
2020-05-26 13:07:02 +0530

    Add homepage route

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Homepage from '../containers/Homepage';
+import { ROUTES } from '../constant';
+
+const routes = (
+  <Switch>
+    <Route path={ROUTES.HOMEPAGE} exact component={Homepage} />
+    <Redirect from="/*" to={ROUTES.HOMEPAGE} />
+  </Switch>
+);
+
+export default routes;
