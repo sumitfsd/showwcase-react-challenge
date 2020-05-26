0ea15ee6e9045c6610281bb49f23e5ca68eaed06
Sumit Rajput <sumitfsd@gmail.com>
2020-05-26 15:36:38 +0530

    Add navigation to root reducer

--- src/app/rootReducer.js
+++ src/app/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import homePage from '../containers/Homepage/reducer';
+import navigation from '../components/Navigation/reducer'
 
 export default combineReducers({
-  user: homePage
+  user: homePage,
+  navigation
 });
