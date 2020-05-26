9b87063ac4565f5f6f346472e171343204093aa8
Sumit Rajput <sumitfsd@gmail.com>
2020-05-26 15:35:42 +0530

    Add styling to homepage

--- src/containers/Homepage/Homepage.jsx
+++ src/containers/Homepage/Homepage.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { MdLocationCity } from 'react-icons/md'
+import { ContentWrapper, Container } from './Homepage.styled'
+import Navigation from '../../components/Navigation';
 
-function Homepage({setUserName, userName}) {
+function Homepage({ setUserName, userName, history }) {
   return (
-    <>HOMEPAGE</>
+    <Container>
+      <Navigation
+        history={history}
+        links={[
+          {
+            text: 'Home',
+            to: `/`,
+            icon: MdLocationCity
+          },
+        ]}
+      />
+      <ContentWrapper>
+        <>Homepage</>
+      </ContentWrapper>
+    </Container>
   )
 }
 
---
+++ src/containers/Homepage/Homepage.styled.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import theme from "../../styles/theme";
+
+export const ContentWrapper = styled.div`
+  background-color: ${theme.grey};
+  flex: 0.9;
+`
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  height: 100%;
+`
